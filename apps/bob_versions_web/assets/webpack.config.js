44428e1ae670b22be0d0f75d1fa3cec7eb19e3d4
Benjamin Milde <benni@kobrakai.de>
2018-12-31 20:05:47 +0100

    Cleanup css/styling and a few html bits

--- apps/bob_versions_web/assets/webpack.config.js
+++ apps/bob_versions_web/assets/webpack.config.js
@@ -29,6 +29,10 @@ module.exports = (env, options) => ({
         }
       },
       {
+        test: /\.s(c|a)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
