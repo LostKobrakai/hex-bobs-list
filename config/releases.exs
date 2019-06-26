a4db6903891031938ec2c109878e06aea6b31060
Benjamin Milde <benni@kobrakai.de>
2019-06-26 22:52:01 +0200

    Simplify releases config

--- config/releases.exs
+++ config/releases.exs
@@ -7,8 +7,5 @@ config :bob_versions, BobVersions.Repo,
   pool_size: 2
 
 config :bob_versions_web, BobVersionsWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  server: true,
   secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
