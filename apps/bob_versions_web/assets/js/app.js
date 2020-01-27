85808f36134160a71b8b7b1a73fd06d5ef759e3e
Benjamin Milde <benni@kobrakai.de>
2020-01-27 12:24:42 +0100

    Impl navbar burger in js
    Close #2

--- apps/bob_versions_web/assets/js/app.js
+++ apps/bob_versions_web/assets/js/app.js
@@ -15,3 +15,19 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+// Bulma Navbar
+
+
+// Get all "navbar-burger" elements
+const navbarBurgers = Array.from(document.querySelectorAll('.navbar-burger'));
+
+// Add a click event on each of them
+navbarBurgers.forEach(el => {
+	el.addEventListener('click', () => {
+		const target = document.getElementById(el.dataset.target);
+		// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+		el.classList.toggle('is-active');
+		target.classList.toggle('is-active');
+	});
+});
