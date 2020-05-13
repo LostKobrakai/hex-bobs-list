6abee5c7b0ce26a49d94e48aeea16e6c7cbe5899
Benjamin Milde <benni@kobrakai.de>
2020-05-13 20:32:03 +0200

    Fix a bug, where statuses would be atoms not strings

--- apps/bob_versions_web/lib/bob_versions_web/live/availability_live.ex
+++ apps/bob_versions_web/lib/bob_versions_web/live/availability_live.ex
@@ -1,5 +1,6 @@
 defmodule BobVersionsWeb.AvailabilityLive do
   use Phoenix.LiveView
+  require Logger
 
   def render(assigns) do
     ~L"""
@@ -11,12 +12,17 @@ defmodule BobVersionsWeb.AvailabilityLive do
   defp status("unavailable"), do: "status--danger"
   defp status("undetermined"), do: "status--unknown"
 
+  defp status(status) do
+    Logger.warn("Invalid status: #{status}")
+    "status--unknown"
+  end
+
   def mount(_params, %{"availability" => availability, "url" => url}, socket) do
     Phoenix.PubSub.subscribe(BobVersions.PubSub, "availability")
 
     availability =
       if connected?(socket) do
-        BobVersions.Availability.url_availability(url)
+        BobVersions.Availability.url_availability(url) |> Atom.to_string()
       else
         availability
       end
