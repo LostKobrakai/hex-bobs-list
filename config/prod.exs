fed7550f3234bb2134331d5aad7fe5ff263b373c
Benjamin Milde <benni@kobrakai.de>
2020-05-13 19:53:18 +0200

    Redirect to https

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,8 @@ config :logger, level: :info
 
 config :bob_versions_web, BobVersionsWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
-  secret_key_base: "${SECRET_KEY_BASE}"
+  secret_key_base: "${SECRET_KEY_BASE}",
+  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil]
