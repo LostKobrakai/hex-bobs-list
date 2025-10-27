1ecd115741a1853ac9c5b1b46dd71b538f585844
Benjamin Milde <benni@kobrakai.de>
2025-10-27 19:40:41 +0100

    Decrease pool sizes

--- lib/bob_versions/support/finch.ex
+++ lib/bob_versions/support/finch.ex
@@ -3,8 +3,8 @@ defmodule BobVersions.Finch do
     Finch.child_spec(
       name: __MODULE__,
       pools: %{
-        :default => [size: 10, count: 2],
-        "https://repo.hex.pm" => [size: 32, count: 8]
+        :default => [],
+        "https://repo.hex.pm" => [size: 4, count: 1]
       }
     )
   end
